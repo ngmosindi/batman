00e3d1a8f59e12bc5e3d16c10f5bfc8bfcc59106
ngoziokon <ngmosindi@yahoo.com>
2022-06-10 03:32:17 +0100

    this must work

---
+++ thisworks.sh
@@ -0,0 +1,17 @@
+git switch <new-nranch name>  (you can also use git checkout)
+git status
+git add playlist
+git commit -m "mesage "
+
+now you will see HEAD --> new branch (not master)
+
+git commit -a - m "meesage"  (adds all unstaged commits")
+
+be careful where you branch from. it matters. 
+
+CHECKOUT
+git switch -c <branch name>  (create a brancha nd switch to it)
+or use
+git checkout -b <branchname>
+
+NOTE!!!
